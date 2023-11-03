3be98e741b500056099d9809f252b5c3e4c6ec81
Abigiel <abigailtwg@gmail.com>
2023-11-03 12:21:17 -0400

    first commit

---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class DealershipFileManager {
+
+    Dealership dealership= new Dealership("SA Motors","Charlotte, NC", "1-800-5555");
+
+    public Dealership getDealership(){
+        return dealership;
+    }
+    public void saveDealership(){
+
+    }
+}
